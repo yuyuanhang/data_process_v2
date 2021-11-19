f501cc3640e9a17cc3911005965523c8d518af33
Yuanhang Yu <yuyuanhang14@163.com>
2021-11-19 16:23:55 +0800

    Add files via upload

---
+++ read_csv/utility/command_parser.h
@@ -0,0 +1,18 @@
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class CommandParser {
+private:
+    std::vector<std::string> tokens_;
+
+public:
+    CommandParser(const int argc, char **argv);
+    const std::string getCommandOption(const std::string &option) const;
+    bool commandOptionExists(const std::string &option) const;
+};
+
+#endif
